696db7aba0916012d72a8c8749da499751ac9bad
James Gardner <jadgardner@protonmail.com>
2025-11-26 10:22:55 +0000

    Remove XPU configuration from setup.py
    Remove XPU configuration check from setup.

--- setup.py
+++ setup.py
@@ -104,8 +104,6 @@ if torch.cuda.is_available():
     extension_type, extension_file, build_name, compiler_args, link_args, detected_arch = configure_cuda()
 elif torch.mps.is_available():
     extension_type, extension_file, build_name, compiler_args, link_args, detected_arch = configure_mps()
-elif hasattr(torch, 'xpu'):
-    extension_type, extension_file, build_name, compiler_args, link_args, detected_arch = configure_xpu()
 else:
     extension_type, extension_file, build_name, compiler_args, link_args, detected_arch = configure_cuda()
 
